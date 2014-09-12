d4df6895b72365b8379c80f9600386e9270248f1
acaetano <ailtoncaetanos@gmail.com>
2014-09-12 15:53:17 -0300

    adding dump_headers.py

---
+++ dump_headers.py
@@ -0,0 +1,11 @@
+
+def response(context, flow):
+
+    fp = open('result','w+')
+    fp.write('Server Response Headers:\n')
+    fp.write('========================\n')
+    for header, value in flow.response.headers.items():
+        fp.write(str(header)+': '+str(value)+'\n')
+    fp.write('\n')
+    fp.close()
+
